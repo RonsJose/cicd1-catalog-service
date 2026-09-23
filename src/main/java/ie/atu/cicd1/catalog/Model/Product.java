f87b9088c59fad792cf7af9aa6f9f24e195bc238
RonsJose-STUDENT <g00464215@atu.ie>
2026-09-23 13:30:05 +0100

    Refractored service to use repository, and removed the ability for users to set the id of an product

--- src/main/java/ie/atu/cicd1/catalog/Model/Product.java
+++ src/main/java/ie/atu/cicd1/catalog/Model/Product.java
@@ -21,8 +21,7 @@ public class Product {
 
     public Product() {}
 
-    public Product(Long id, String name, BigDecimal price) {
-        this.id = id;
+    public Product(String name, BigDecimal price) {
         this.name = name;
         this.price = price;
     }
--- src/main/java/ie/atu/cicd1/catalog/Service/ProductService.java
+++ src/main/java/ie/atu/cicd1/catalog/Service/ProductService.java
@@ -1,22 +1,27 @@
 package ie.atu.cicd1.catalog.Service;
 
 import ie.atu.cicd1.catalog.Model.Product;
+import ie.atu.cicd1.catalog.Repository.ProductRepository;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
+
 import java.util.List;
+
 
 @Service
 public class ProductService {
-    private final List<Product> products = new ArrayList<>();
-    private long nextId = 1;
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public List<Product> getAll() {
-        return products;
+        return productRepository.findAll();
     }
 
     public Product create(Product product) {
-        product.setId(nextId++);
-        products.add(product);
-        return product;
+        product.setId(null);
+        return productRepository.save(product);
     }
 }
