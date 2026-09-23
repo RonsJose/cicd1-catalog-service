3a7fff36459fb2b50afdde4addfb8c89cd35bff1
RonsJose-STUDENT <g00464215@atu.ie>
2026-09-23 13:14:39 +0100

    Converted Product into a JPA entity

--- src/main/java/ie/atu/cicd1/catalog/Model/Product.java
+++ src/main/java/ie/atu/cicd1/catalog/Model/Product.java
@@ -1,5 +1,6 @@
 package ie.atu.cicd1.catalog.Model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,14 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private BigDecimal price;
 
